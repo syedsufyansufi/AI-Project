b01265b89361fdd991eea902c8aec31ce2fa7c98
syedsufyansufi <syedsufiyan787@gmail.com>
2025-08-26 20:51:14 +0530

    feat: add UI components and styles with Tailwind CSS integration

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [massage, setMassage] = useState("");
@@ -11,9 +12,10 @@ function App() {
       .then((data) => setMassage(data.massage));
   }, []);
   return (
-    <>
-      <p>{massage}</p>
-    </>
+    <div className="p-4">
+      <p className="font-bold text-3xl">{massage}</p>
+      <Button variant="outline">Click Me</Button>
+    </div>
   );
 }
 
